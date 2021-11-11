5b89054676fec56c92ecf897b318a1b1892313cd
cromeo98 <romeo.christian98@gmail.com>
2021-11-11 14:36:54 +0100

    Aggiungere i file di progetto.

---
+++ Exercise1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fiscalCode;
+            Console.WriteLine("inserisci il tuo codice fiscale");
+            fiscalCode = Console.ReadLine();
+            //Console.WriteLine(fiscalCode);
+        }
+    }
+}
